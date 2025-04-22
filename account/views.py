76d6fc635c0987e959220486351c70a195595edb
Əbil Abilli <ebilebilli3@gmail.com>
2025-04-23 02:01:52 +0400

    Set up multiple servers and added different containers for books in Docker.

--- account/views.py
+++ account/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer, LoginSerializer, LogoutSerializer
--- todo_project/urls.py
+++ todo_project/urls.py
@@ -21,8 +21,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('app.urls')),
-    path('api/v1/', include('account.urls')),
+    path('api/v1/app/', include('app.urls')),
+    path('api/v1/accounts/', include('account.urls')),
     #documentation
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
